a3fa8eddf935fb070f8b8f6e87a39e50feb981ea
César Gómez <caugustog89@gmail.com>
2024-04-19 14:12:17 -0500

    se actuaiza archivo validacion.js

--- validacion.js
+++ validacion.js
@@ -3,16 +3,27 @@ const btnEnviar = document.getElementById('formcontato__botao');
 
 const validacion = (e) => {
     e.preventDefault();
-    const nombreDeUsuario = document.getElementById('nombre');
-    const direccionEmail = document.getElementById ('Email');
-    const asunto = document.getElementById ('asunto');
-    const mensaje = document.getElementById ('mensaje');
+    const nombreDeUsuario = document.getElementById('nombre').value;
+    const direccionEmail = document.getElementById ('Email').value;
+    const asunto = document.getElementById ('asunto').value;
+    const mensaje = document.getElementById ('mensaje').value;
 
 
-    if (nombreDeUsuario.value === "" || nombreDeUsuario.lenght <= 50){
-        alert ("por favor, escriba aquí su nombre");
+    if (nombreDeUsuario.value === "" || nombreDeUsuario.lenght <= 1 || nombreDeUsuario.lenght >= 50){
+        alert ("por favor, escriba aquí su nombre, ocupar menos de 50 caracteres");
         nombreDeUsuario.focus();
         return false;
+    }
+
+    if (direccionEmail.value === "" || direccionEmail === null || direccionEmail.lenght === 0){
+        alert ("por favor, escriba aquí su E-mail");
+        direccionEmail.focus();
+        return false;
+    }
+
+    if (!(/\S+@\S+\.\S+/.test(direccionEmail))) {
+        TextoError+="Error: debe ingresar un correo valido\n";
+        correcto = false;
     }
 
     if (asunto.value === ""){
@@ -22,12 +33,6 @@ const validacion = (e) => {
     }
 
     if (mensaje.value === ""){
-        alert ("por favor, escriba aquí su E-mail");
-        direccionEmail.focus();
-        return false;
-    }
-
-    if (direccionEmail.value === ""){
         alert ("por favor, escriba aquí su E-mail");
         direccionEmail.focus();
         return false;
