92f277e7feace5d3085f97dfc32b9365d81e5958
César Gómez <caugustog89@gmail.com>
2024-04-19 13:25:03 -0500

    se agrega requiered en los campos del formulario

--- validacion.js
+++ validacion.js
@@ -7,7 +7,9 @@ const validacion = (e) => {
     const direccionEmail = document.getElementById ('Email');
     const asunto = document.getElementById ('asunto');
     const mensaje = document.getElementById ('mensaje');
-    if (nombreDeUsuario.value === ""){
+
+
+    if (nombreDeUsuario.value === "" || nombreDeUsuario.lenght <= 50){
         alert ("por favor, escriba aquí su nombre");
         nombreDeUsuario.focus();
         return false;
