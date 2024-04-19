3173f45686de4a8eede1b838caf31a46b047e963
César Gómez <caugustog89@gmail.com>
2024-04-19 12:58:45 -0500

    se contruye el archivo validacion.js

--- validacion.js
+++ validacion.js
@@ -1,1 +1,36 @@
 //Haz tú validación en javascript acá
+const btnEnviar = document.getElementById('formcontato__botao');
+
+const validacion = (e) => {
+    e.preventDefault();
+    const nombreDeUsuario = document.getElementById('nombre');
+    const direccionEmail = document.getElementById ('Email');
+    const asunto = document.getElementById ('asunto');
+    const mensaje = document.getElementById ('mensaje');
+    if (nombreDeUsuario.value === ""){
+        alert ("por favor, escriba aquí su nombre");
+        nombreDeUsuario.focus();
+        return false;
+    }
+
+    if (asunto.value === ""){
+        alert ("por favor, escriba aquí su E-mail");
+        direccionEmail.focus();
+        return false;
+    }
+
+    if (mensaje.value === ""){
+        alert ("por favor, escriba aquí su E-mail");
+        direccionEmail.focus();
+        return false;
+    }
+
+    if (direccionEmail.value === ""){
+        alert ("por favor, escriba aquí su E-mail");
+        direccionEmail.focus();
+        return false;
+    }
+    return true;
+}
+
+submitbtn.addEventListenner('click', validate);
